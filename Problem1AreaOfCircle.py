1e69176344a9e5291202d450258a4bdd661516b6
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-08-27 05:34:26 -0500

    Update Problem1AreaOfCircle.py

--- Problem1AreaOfCircle.py
+++ Problem1AreaOfCircle.py
@@ -2,7 +2,9 @@
 #08/20/2022
 #Problem number 1 - A function areaOfCircle(r) which returns the area of a circle of radius
 
+
+
 def areaOfCircle(r):
-    from math import pi #this will get the value of pi from math module.
+    from math import pi #this will get the value of pi from math module. <= BETTER TO TAKE OUT FROM FUNCTION
     return pi * r * r #This returns the area of circle which is calculated as pi times r squared
 print(areaOfCircle(1)) #This prints the areaofcircle
